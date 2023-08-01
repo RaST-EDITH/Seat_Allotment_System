620707c6a494e79c5b4609a0974a0e6e6aebeebb
Raghvendra5448 <113932056+Raghvendra5448@users.noreply.github.com>
2023-08-02 00:11:03 +0530

    Login Method setup

--- main.py
+++ main.py
@@ -36,7 +36,27 @@ def firstpage() :
     frpa.create_text(1025,180,text="JSS ADMISSION",font=("Book Antiqua",28,"bold"),fill="#0b4bf5")##0b4bf5
     frpa.create_text(970,305,text="Username",font=("Book Antiqua",22,"bold"),fill="white")
     frpa.create_text(970,505,text="Password",font=("Book Antiqua",22,"bold"),fill="white")
+    
+    # Entry of username and password
+    user=Entry(root,font=("Book Antiqua",20,"bold"),width=17,fg="white",bd=0,bg="#ffca62")
+    pwrd=Entry(root,font=("Book Antiqua",20,"bold"),width=17,fg="white",bd=0,bg="#ffca62")
+    user_win=frpa.create_window(890,355,anchor="nw",window=user)
+    pwrd_win=frpa.create_window(890,555,anchor="nw",window=pwrd)
+    user.insert(0, "Roll Number")
+    pwrd.insert(0, "DOB(dd/mm/yyyy)")
 
+    # Login button
+    log_bt=ctk.CTkButton(master=root, text="Login", text_font=("Book Antiqua",25,"bold"), width=30, height=20,
+                         corner_radius=15, text_color="white", bg_color="#dadada", fg_color="#162d50", 
+                         hover_color="#0c3f8c", border_width=0, command = nxt_widg)
+    log_bt_win=frpa.create_window(960,675,anchor="nw",window=log_bt)
+
+    # binding entry boxes
+    user.bind("<FocusIn>",ent_clr)
+    user.bind("<FocusOut>",ent_place)
+    pwrd.bind("<FocusIn>",ent_clr1)
+    pwrd.bind("<FocusOut>",ent_place1)
+    
     root.mainloop()
 
 if __name__ == "__main__" :
