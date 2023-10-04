f7f99f3f6685fd59a2d283ae565d24fb7d35ab8e
RaST-EDITH <raghvsinghtomar1975@gmail.com>
2023-10-04 18:24:08 +0530

    Date choosing option added

--- main.py
+++ main.py
@@ -238,6 +238,16 @@ def slotpage() :
                          command=lambda : slot_booked(date_var.get() , slot_var.get() , slotp))
         save_bt_win=slotp.create_window(800,640,anchor="nw",window=save_bt)
 
+    else :
+
+        # Drop menu for choosing date
+        date_var = StringVar()
+        date_var.set(ws2[f"C{rec}"].value)
+        date_drop = OptionMenu(slotpa, date_var, ws2[f"C{rec}"].value)
+        date_drop.config(width=15, bg="#ffc234", font=("Book Antiqua",15,"bold"))
+        date_drop.place_configure(x=750+180,y=350,anchor="nw")
+        date_drop.config(state="disable")
+
     # Back button
     back_bt=ctk.CTkButton(master=slotpa, text="BACK", text_font=("Helvetica",20),  width=20, height=10, corner_radius=10,
                         fg_color="red", hover_color="#ff5359", bg_color="#ebebeb", text_color="#f9f9f9", border_width=0,
